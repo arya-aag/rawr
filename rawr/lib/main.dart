426c0c8f38f19cc590e3b14ed9717a4e2be8a09d
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-09-07 01:32:05 +0530

    add image file and make a stateful widget

--- rawr/lib/main.dart
+++ rawr/lib/main.dart
@@ -2,13 +2,39 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) => runApp(MainApp());
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainAppState();
+  }
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Rawr'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text('Food Paradise')
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
