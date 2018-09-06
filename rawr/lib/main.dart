95e6e425f5cbb1b20995f4c391338d91c67fae1a
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-09-07 04:13:29 +0530

    use lifecycle hooks

--- rawr/lib/main.dart
+++ rawr/lib/main.dart
@@ -1,54 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main(List<String> args) => runApp(MainApp());
 
-class MainApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MainAppState();
-  }
-}
-
-class _MainAppState extends State<MainApp> {
-  List<String> _products = ['Food Tester'];
-
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Rawr'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: MaterialButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Awesomeness');
-                  });
-                },
-                child: Text('Add Product'),
-              ),
-            ),
-            Column(
-              children: _products
-                  .map(
-                    (element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpg'),
-                              Text(element)
-                            ],
-                          ),
-                        ),
-                  )
-                  .toList(),
-            )
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Rawr'),
+          ),
+          body: ProductManager(['First Product'])),
     );
   }
 }
---
+++ rawr/lib/product_manager.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+
+class ProductManager extends StatefulWidget {
+  final List<String> initialProducts;
+
+  ProductManager(this.initialProducts) {
+    print('ProductManagerWidget constr');
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    print('ProductManagerWidget createState');
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products;
+
+  @override
+  void initState() {
+    super.initState();
+    print('ProductManagerWidget initState');
+    _products = widget.initialProducts;
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('ProductManagerWidget didUpdateWidget');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('ProductManagerWidget build');
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: MaterialButton(
+            onPressed: () {
+              setState(() {
+                _products.add('Advanced Awesomeness');
+              });
+            },
+            child: Text('Add Product'),
+          ),
+        ),
+        Products(_products)
+      ],
+    );
+  }
+}
