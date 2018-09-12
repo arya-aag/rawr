6b3bc9e23f1122779431035bb4ff86ad52c35c77
Arya Adarsha Gautam <arya-aag@users.noreply.github.com>
2018-09-12 09:41:15 +0530

    navigate from side drawer

--- rawr/lib/pages/products.dart
+++ rawr/lib/pages/products.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 import '../product_manager.dart';
+import './products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage products'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProductsAdminPage()),
+                );
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('EasyList'),
       ),
---
+++ rawr/lib/pages/products_admin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductsAdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Products Admin'),
+      ),
+      body: Center(
+        child: Text('Admin page is here!'),
+      ),
+    );
+  }
+}
